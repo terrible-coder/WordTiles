a4a2c8c4f50d521de6ace1e95c94cb3f22c351d2
Ayanava De <ayanavade01@gmail.com>
2021-05-21 21:57:18 +0530

    feat(server): :speech_balloon: commands
    Adding few basic commands with their descriptions. Added listeners to each.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Bot } from "grammy";
 require("dotenv").config();
 
+import { command_list } from "./core.bot/commands";
+
 /**
  * The authentication token for the Telegram Bot API.
  * 
@@ -10,8 +12,25 @@ const TG_TOKEN = <string>process.env.TG_TOKEN;
 
 const bot = new Bot(TG_TOKEN);
 
+(async () => {
+	await bot.api.setMyCommands(Object.keys(command_list).map(command => {
+		return {
+			command: command,
+			description: command_list[command].desc
+		}
+	}));
+})();
+
+Object.keys(command_list).forEach(command => {
+	bot.command(command, ctx => {
+		const message = command_list[command].message;
+		message.forEach(msg => ctx.reply(msg));
+	});
+});
+
 bot.command("start", ctx => ctx.reply("Welcome to the game player."));
 bot.on("message:text", ctx => ctx.reply("Echo: " + ctx.message.text));
 
 
 bot.start();
+console.log("bot online");
