3b9235a47d361ab8576fb053acf739270f878b3a
Ayanava De <ayanavade01@gmail.com>
2021-05-24 19:09:01 +0530

    feat(db): :card_file_box: redundancy
    Checking database if object already exists and updates it

--- src/core/data.ts
+++ src/core/data.ts
@@ -12,10 +12,17 @@ const playerData = new Datastore({ filename: "./data/players.db" });
 playerData.loadDatabase();
 
 export function addPlayer(player: PlayerData) {
-	playerData.insert(player, err => {
-		if(err !== null)
-			if((<any>err).errorType === "uniqueViolated")
-				console.log("Player already exists.");
+	playerData.find({ _id: player._id }, (_: any, docs: PlayerData[]) => {
+		if(docs.length === 0)
+			playerData.insert(player);
+		else {
+			const p = docs[0];
+			const id = p._id;
+			if(player.user.username !== p.user.username) {
+				playerData.update({ _id: id, "user.username": p.user.username },
+						  { $set: { _id: id, "user.username": player.user.username } });
+			}
+		}
 	});
 	console.log("player added");
 }
